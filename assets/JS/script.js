8cc23adf708e758113587bbca063b5a1244b4c26
tobias-firth <tobyfirth@btinternet.com>
2023-01-04 19:37:41 +0000

    add pseudocode for JS

---
+++ assets/JS/script.js
@@ -0,0 +1,36 @@
+// Click button to update favorite team
+    // add event listener for when button is clicked, a modal appears requesting the user to enter their favourite team and their nearest train station
+
+// Store user input favourite team as a variable
+    // Store this in local storage
+
+
+// Click button to update location
+// add event listener for when button is clicked, a modal appears
+
+// Store user input of location as a variable
+    // getelementbyid value from input
+    // Store this in local storage
+
+
+// Store todays date as variable in format YYYY-MM-DD
+    // var today = moment().format()
+
+// Print the club logo of favourite team in top corner of the header
+    // get the logo from the api
+    // add img element with src attribute
+
+
+// Print the next eight fixtures for the team in the aside
+    // Store football api key
+    // Store football api URL
+
+    // Get next 8 fixtures for the team
+    // For loop, insert a list element for each match and a button to plan the journey
+
+
+// When user clicks on the "plan" button, plan journey from users home location to location of the match
+// Print map of journey
+// Print stages of journey
+
+// Add saved journeys onto the page??
