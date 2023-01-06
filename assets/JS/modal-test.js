9e9c1c7ba4ce53e02f1db96e2b8e2f75e24cc704
adamweldonming-ymHwsk <adamweldon@gmail.com>
2023-01-06 08:03:35 +0000

    created the Modal for Teams and Postcode

--- assets/JS/modal-test.js
+++ assets/JS/modal-test.js
@@ -1,62 +1,35 @@
-// Array of teams
-var teams = [
-    "Arsenal",
-    "Aston Villa",
-    // Add names of other teams
-  ];
+var updateTeamBtn = document.getElementById("updateTeamBtn");
+var teamForm = document.getElementById("teamForm");
+var teamSelect = document.getElementById("teamSelect");
+var updatePostcodeBtn = document.getElementById("updatePostcodeBtn");
+var postcodeForm = document.getElementById("postcodeForm");
+var postcodeInput = document.getElementById("postcodeInput");
 
-// Get the modal element
-var modal = document.getElementById("myModal");
+// An array of English Premier League teams
+var teams = ["Arsenal", "Aston Villa", "Brentford", "Brighton & Hove Albion", "Burnley", "Chelsea", "Crystal Palace", "Everton", "Leeds United", "Leicester City", "Liverpool", "Manchester City", "Manchester United", "Newcastle United", "Norwich City", "Southampton", "Tottenham Hotspur", "Watford", "West Ham United", "Wolverhampton Wanderers"];
 
-// Get the button that opens the modal
-var btn = document.getElementById("updateBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-// Get the form element
-var form = document.getElementById("teamForm");
-
-// When the user submits the form, store the team in local storage
-form.onsubmit = function(event) {
-  // Prevent the form from submitting
-  event.preventDefault();
-
-  // Get the selected team
-  var select = document.getElementById("teamSelect");
-  var team = select.options[select.selectedIndex].value;
-
-  // Store the team in local storage
-  localStorage.setItem("favouriteTeam", team);
-
-  // Close the modal
-  $('#myModal').modal('hide');
-}
-
-// Get the select element
-var select = document.getElementById("teamSelect");
-
-// Add an option element for each team
-teams.forEach(team => {
+// Populate the select element with the team options
+teams.forEach(function(team) {
   var option = document.createElement("option");
   option.value = team;
   option.text = team;
-  select.add(option);
+  teamSelect.appendChild(option);
 });
+
+// Save the selected team to local storage when the form is submitted
+teamForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  localStorage.setItem("favouriteTeam", teamSelect.value);
+
+  // Close the modal
+  $("#teamModal").modal("hide");
+});
+
+// Save the entered postcode to local storage when the form is submitted
+postcodeForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  localStorage.setItem("postcode", postcodeInput.value);
+
+  // Close the modal
+  $("#postcodeModal").modal("hide");
+  });
